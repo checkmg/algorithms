76d42b7b66ad2ddae8888518b086eeb8907e18b8
mgupta <mgupta@lendingclub.com>
2018-08-08 23:29:08 -0700

    Adding Bubble and merge sort algo examples

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSort {
+
+	public List<Integer> sort(List<Integer> input) {
+		boolean swapped = false;		
+		for(int i=0; i < input.size(); i++) {
+			if(i != input.size()-1) {
+				if(input.get(i) >= input.get(i+1)) {
+					Integer temp = input.get(i+1);
+					input.set(i+1, input.get(i));
+					input.set(i, temp);
+					swapped = true;
+				}	
+			}
+		}
+		if(swapped) {
+			input = sort(input);
+		}
+		return input;
+	}
+	
+	public static void main(String args[]) {
+		Integer[] a = {Integer.valueOf(5),Integer.valueOf(3),Integer.valueOf(1)};
+		BS bs = new BS();
+		List<Integer> sorted = bs.sort(Arrays.asList(a));
+		System.out.println(sorted);
+	}
+	
+}
+
---
+++ MergeSort.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSort {
+
+	public List<Integer> ms(List<Integer> input) {
+		if(input.size() <= 1)
+			return input;
+		int index = input.size()/2;
+		List<Integer> left = new ArrayList();
+		List<Integer> right = new ArrayList();
+		for(Integer a : input) {
+			if(a.intValue() <= input.get(index)) {
+				left.add(a);
+			} else {
+				right.add(a);
+			}				
+		}
+		left = ms(left);
+		right = ms(right);
+		return merge(left, right);
+	}
+	
+	public List<Integer> merge(List<Integer> left, List<Integer> right) {
+		List<Integer> result = new ArrayList();
+		Integer i = 0;
+		while(i < left.size()) {
+			if(left.get(i) <= right.get(i)) {
+				result.add(left.get(i));
+				left.remove(left.get(i));
+			} else {
+				result.add(right.get(i));
+				right.remove(right.get(i));
+			}
+		}
+		if(left.size() > 0)
+			result.addAll(left);
+		if(right.size() > 0)
+			result.addAll(right);
+		return result;
+	}
+	
+	public static void main(String args[]) {
+		Integer[] a = {Integer.valueOf(5),Integer.valueOf(3),Integer.valueOf(1)};
+		MS ms = new MS();
+		List<Integer> sorted = ms.ms(Arrays.asList(a));
+		System.out.println(sorted);
+	}
+}
+
