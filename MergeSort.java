f9f82b8087f28b2e573b56edeadbec650c27c3b5
checkmg <checkmg@yahoo.com>
2018-08-14 11:07:40 -0700

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-
+        //Complexity of NlogN
 	public List<Integer> ms(List<Integer> input) {
 		if(input.size() <= 1)
 			return input;
 		int index = input.size()/2;
 		List<Integer> left = new ArrayList();
 		List<Integer> right = new ArrayList();
+		//logN Complexity
 		for(Integer a : input) {
 			if(a.intValue() <= input.get(index)) {
 				left.add(a);
@@ -26,6 +27,7 @@ public class MergeSort {
 	public List<Integer> merge(List<Integer> left, List<Integer> right) {
 		List<Integer> result = new ArrayList();
 		Integer i = 0;
+		//N Complexity
 		while(i < left.size()) {
 			if(left.get(i) <= right.get(i)) {
 				result.add(left.get(i));
